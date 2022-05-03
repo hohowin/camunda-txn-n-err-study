3816fa37689360e81e09f6a86679368df97e6330
Howin Ho <ho1ho@yahoo.com>
2022-05-03 17:16:01 -0400

    integrated

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # camunda-txn-n-err-study
+
+References:
+- Medium - [Combine Multiple Springboot Applications into a Single Application](https://medium.com/@eranda/combine-multiple-springboot-applications-into-a-single-application-649514e7a447)
+- Youtube - [Create an IntelliJ project with Sub Modules & Fix project structure | IntelliJ Tutorial](https://youtu.be/4WJ3b2GTDko)
+- Kong - [Maven – How to create a multi module project](https://mkyong.com/maven/maven-how-to-create-a-multi-module-project/)
+- Oracle - [Building a Real Application with Maven](https://docs.oracle.com/middleware/1212/core/MAVEN/real_app.htm#MAVEN8917)
+
+## To Run the Applications
+
+1\. Start Postgres
+
+```bash
+docker-compose up -d
+```
+
+2\. Start the Student Applications by running `com.example.demo.DemoApplication`
+
+3\. Can verify by browsing `http://http://localhost:8080/api/v1/student`
+
+4\. Start Camunda by running `example.workflow.Application`
+
+5\. Login to Camunda. `http://localhost:8088`
+
+## To Shut Down 
+
+Stop the `Applications` and `docker compose`.
--- camunda/src/main/java/example/workflow/MyBeans.java
+++ camunda/src/main/java/example/workflow/MyBeans.java
@@ -1,10 +1,18 @@
 package example.workflow;
 
+import com.example.demo.student.Student;
+import com.example.demo.student.StudentRepository;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.Random;
+
 @Configuration
+@ComponentScan(basePackages = {"com.example.demo"})
 public class MyBeans {
 
     @Bean
@@ -12,4 +20,18 @@ public class MyBeans {
         return execution -> System.out.println("\n\nAhoy " + execution.getVariable("myVariable") + "!");
     }
 
+    @Bean
+    public JavaDelegate createStudent(StudentRepository repository) {
+        return execution -> {
+            int randNum = (new Random()).nextInt(100);
+            String name = execution.getVariable("studentName") + Integer.valueOf(randNum).toString();
+
+            Student newStudent = new Student(
+                    name,
+                    LocalDate.of(2022 - randNum, Month.JANUARY, 10),
+                    name + "@yahoo.com"
+            );
+            repository.save(newStudent);
+        };
+    }
 }
