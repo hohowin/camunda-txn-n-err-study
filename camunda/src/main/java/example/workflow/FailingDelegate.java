9741b16c0b4e89a2e00f9bc598e0115df315835b
Howin Ho <ho1ho@yahoo.com>
2022-05-04 15:32:08 -0400

    touch up the flow

--- camunda/src/main/java/example/workflow/FailingDelegate.java
+++ camunda/src/main/java/example/workflow/FailingDelegate.java
@@ -11,7 +11,7 @@ public class FailingDelegate implements JavaDelegate {
     @Override
     public void execute(DelegateExecution execution) throws Exception {
         System.out.println("I am throwing MySpecialError....");
-        throw new BpmnError("MySpecialError", "I like to produce error.");
-//        throw new RuntimeException("I am throwing MySpecialError");
+//        throw new BpmnError("MySpecialError", "I like to produce error.");
+        throw new RuntimeException("I am throwing MySpecialError");
     }
 }
